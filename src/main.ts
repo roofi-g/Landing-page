53d1c64c2cebca7ae06b6971821e6a6fe4a3e306
Roofi <roofi.g@yandex.ru>
2026-02-08 18:59:54 +0500

    header + section-hero

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,1 @@
-
+import './modules/changeHeaderOnScroll';
---
+++ src/modules/changeHeaderOnScroll.ts
@@ -0,0 +1,7 @@
+const header = document.querySelector<HTMLElement>('.header');
+
+if (header) {
+  window.addEventListener('scroll', () => {
+    header.classList.toggle('scrolled', window.scrollY > 50);
+  });
+}
