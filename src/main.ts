f9fe936f013f1292116c36e03ead4ca797f9e29f
Roofi <roofi.g@yandex.ru>
2026-02-11 11:59:40 +0500

    validate form

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@ import './modules/changeHeaderOnScroll';
 import './modules/getFullYear';
 
 import './modules/openModalWindow';
+import './modules/validateForm';
 import './modules/closeModalWindow';
---
+++ src/modules/validateForm.ts
@@ -0,0 +1,80 @@
+class validateForm {
+  constructor(form) {
+    this.form = form;
+    this.inputs = form.querySelectorAll('.js-validate')
+  }
+
+  init() {
+    this.form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const isValid = this.validateForm();
+
+      if (isValid) {
+        this.onSuccess();
+      }
+    });
+  }
+
+  validateForm() {
+    let isValid = true;
+    this.inputs.forEach(input => {
+      
+      this.clearError(input);
+      const value = input.value.trim();
+
+      if (!value) {
+        this.setError(input, 'Пожалуйста, заполните поле');
+        isValid = false;
+        return;
+      }
+
+      if (input.classList.contains('name') && value.length < 2) {
+        this.setError(input, 'Короткое имя');
+        isValid = false;
+        return;
+      }
+
+      if (input.classList.contains('email')) {
+        const ok = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+        if (!ok) {
+          this.setError(input, 'Некорректный email');
+          isValid = false;
+          return;
+        }
+      }
+
+      if (input.classList.contains('message') && value.length < 15) {
+        this.setError(input, 'Короткое сообщение');
+        isValid = false;
+        return;
+      }
+    });
+
+    return isValid;
+  }
+
+  setError(input, message) {
+    const field = input.closest('.field');
+    const error = field.querySelector('.error-message');
+    
+    input.classList.add('error');
+    error.textContent = message;
+  }
+
+  clearError(input) {
+    const field = input.closest('.field');
+    const error = field.querySelector('.error-message');
+    
+    input.classList.remove('error');
+    error.textContent = '';
+  }
+
+  onSuccess() {
+    console.log('Форма валидна');
+    this.form.reset();
+  }
+}
+
+const form = document.getElementById('modalForm');
+const validator = new validateForm(form);
+validator.init();
